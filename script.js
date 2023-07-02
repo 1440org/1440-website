39480fcddbade45094f8013e9ebda19b10890302
Nassar Hayat <nassar@makerlabs.co.uk>
2023-07-02 19:37:01 +0100

    minutes left adjustment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(function() {
         const now = new Date();
         const minutesPassed = now.getHours() * 60 + now.getMinutes();
-        document.getElementById('minutes-passed').innerHTML = `${minutesPassed} / 1440`;
+        const minutesLeft = 1440 - minutesPassed;
+        document.getElementById('minutes-passed').innerHTML = `${minutesLeft} / 1440`;
     }, 1000);
 });
